ebe2e82df1ab54e9b0c73908884afedbd33cc7a1
evandrvalente <evandr.valente@gmail.com>
2021-12-14 16:48:19 -0300

    requisito 9 finalizado

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const taskEditor = document.getElementById("texto-tarefa");
 const addTaskBtn = document.getElementById("criar-tarefa");
 const taskList = document.getElementById("lista-tarefas");
-const tList = document.getElementsByClassName("toDo");
+const tList = document.getElementsByTagName("li");
+const delAllBtn = document.getElementById("apaga-tudo");
+const delDoneBtn = document.getElementById("remover-finalizados");
 const d0 = 1639442823951;
 
 //criar função para adicionar tarefa à lista
@@ -10,14 +12,18 @@ function addTask (){
     let newTask = taskEditor.value;
     let newElement = document.createElement("li");
     newElement.innerText = newTask;
-    newElement.classList.add("toDo");
+    /*Queria saber se era possível atribuir um ID já na 
+    criação do elemento, só de curiosidade mesmo. */
     const dN = Date.now();
     const nID = dN - d0;
     const taskId = "task" + nID;
     newElement.setAttribute("id", taskId);
     taskList.appendChild(newElement);
-    //testei incluir um eventlistenner aqui, confesso que isso oi no chutem lance de sorte
+    /*Testei incluir um eventlistenner ao elemento já na criação,
+     já que colocar outros atributos deu certo, 
+     confesso que isso foi no chute, lance de sorte*/
     newElement.addEventListener("click", handleChangeTask);
+    newElement.addEventListener("dblclick", checkTask);
     taskEditor.value = "";
 };
 
@@ -32,4 +38,30 @@ function handleChangeTask(event){
     };
     event.target.classList.add("selected-task");
 };
- 
+
+//completed 
+function checkTask(event){
+    
+        if (event.target.className === "completed" || event.target.className === "completed selected-task") {
+            event.target.removeAttribute("class" , "completed");
+        } else {
+            event.target.setAttribute("class", "completed");
+        };
+    
+};
+
+//limpar lista
+function clearAll(){
+    while (taskList.hasChildNodes()){
+        taskList.removeChild(taskList.firstChild);
+    };
+};
+
+//limpar somente os finalizados
+
+let c = document.querySelectorAll('.completed');
+function clearCompleted(){
+    for (let s = 0; s < c.length; s += 1) {
+        console.log(c[s]);
+    };
+};
